b7f8251adbf51a993878c5df76c0ba4c54a6813f
Martin N <mr.avgp@googlemail.com>
2012-07-15 08:37:44 -0400

    Initial commit.

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express'), 
+http = require('http');
+
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(process.env.PORT);
+console.log(process.env.PORT);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+    socket.on("imgData", function(data) {
+        console.log("Received data. Broadcasting..");
+        this.broadcast.emit("imgData", data);
+    });
+});
